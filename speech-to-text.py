4b1acf1072ec5567c5e4bffc80e10f6ff9e856a9
DataMonarch <toghrultahirov2@gmail.com>
2023-03-20 00:13:55 +0400

    Refactoring

--- speech-to-text.py
+++ speech-to-text.py
@@ -14,3 +14,25 @@ except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
     print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+def recognize_speech(file_path, recognizer, language="en-US"):
+    """Transcribe speech from a recording.
+    Returns a dictionary with three keys:
+    "success": a boolean indicating whether or not the API request was
+               successful
+    "error":   `None` if no error occured, otherwise a string containing
+               an error message if the API could not be reached or
+               speech was unrecognizable
+    "transcription": `None` if speech could not be transcribed,
+                     otherwise a string containing the transcribed text
+    """
+    # check that recognizer and microphone arguments are appropriate type
+    if not isinstance(recognizer, sr.Recognizer):
+        raise TypeError("`recognizer` must be `Recognizer` instance")
+
+
+
+    # adjust the recognizer sensitivity to ambient noise and record audio
+    # from the microphone
+    with microphone as source:
+        recognizer.adjust_for_ambie
